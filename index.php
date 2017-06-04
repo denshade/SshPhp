3d193f6af9b0c40d3a5631a9b6b9a0b970966b1e
denshade <lveeckha@ai.internal.uzgent.be>
2017-06-04 18:12:30 +0200

    first

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    <style type="text/css">
+textarea {
+    width: 100%;
+ background-color: black;
+ color: white;
+ font-size: 1em;
+ font-family: Courier, sans-serif;
+ border: 1px solid black;
+}
+.inputline {
+    width: 100%;
+ background-color: black;
+ color: white;
+ font-size: 1em;
+ font-family: Verdana, Arial, Helvetica, sans-serif;
+ border: 1px solid black;
+}
+</style>
+    </head>
+    <body color="white">
+        Host  <input type="text"/><br/>
+        Login <input type="text"/><br/>
+        Pwd   <input type="text"/><br/>
+
+        <textarea id="textarea" rows="20"><?php
+        $cmd = filter_input(INPUT_GET, 'cmd', FILTER_SANITIZE_ENCODED);
+        if (isset($cmd) && $cmd != "")
+        {
+            exec($cmd, $output, $err);
+            file_put_contents("logs", "$cmd>\n", FILE_APPEND);
+            file_put_contents("logs", $output, FILE_APPEND);
+            echo file_get_contents("logs");            
+        }
+
+        ?>
+        </textarea><br/>
+        <form name="subm" action="index.php" method="GET">
+        <input class="inputline" id="cmd" name="cmd" type="text"/>
+        </form>
+        <script >var textarea = document.getElementById('textarea');
+                textarea.scrollTop = textarea.scrollHeight;</script>
+    </body>
+</html>
